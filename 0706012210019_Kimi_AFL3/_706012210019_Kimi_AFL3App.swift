602532bbf6d9f86814f37a0f0e26512a3d9f5438
kimiharisen <kimi.harisen.1911@gmail.com>
2024-04-19 10:09:59 +0700

    Initial Commit

---
+++ 0706012210019_Kimi_AFL3/_706012210019_Kimi_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012210019_Kimi_AFL3App.swift
+//  0706012210019_Kimi_AFL3
+//
+//  Created by MacBook Pro on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct _706012210019_Kimi_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
